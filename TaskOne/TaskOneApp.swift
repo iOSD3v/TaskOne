9cf663d462c9bd99524e9658607f3a5aaeed3217
iOSD3v <nikkhah.fahimeh@gmail.com>
2024-09-01 14:52:27 +0330

    Initial Commit

---
+++ TaskOne/TaskOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskOneApp.swift
+//  TaskOne
+//
+//  Created by Fahimah on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
